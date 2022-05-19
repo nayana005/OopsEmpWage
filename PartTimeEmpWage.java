e37736824597231d29e41493ea0d5a64b44cb4d7
nayana005 <nayanasuresh1005@gmail.com>
2022-05-20 01:27:42 +0530

    Added Part Time Emp Wage

---
+++ PartTimeEmpWage.java
@@ -0,0 +1,33 @@
+package com.bl.oopsEmpWage;
+
+public class PartTimeEmpWage {
+	public static void main(String[] args) {
+		System.out.println("Welcome to EmployeeWageProblem");
+
+		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
+		int EMP_RATE_PER_HOUR = 20;
+		int empHrs = 0;
+		int empWage = 0;
+		double empCheck = Math.floor(Math.random()*10)%3;
+
+		if (empCheck == IS_FULL_TIME) {
+			System.out.println("Employee is Present");
+			empHrs = 8;
+		}
+
+		else if (empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Present");
+			empHrs = 4;
+		}
+
+		else {
+			System.out.println("Employee is Absent");
+			empHrs = 0;
+		}
+
+		empWage = empHrs*EMP_RATE_PER_HOUR;
+		System.out.println("Emp Wage:" + empWage);
+	}
+
+}
